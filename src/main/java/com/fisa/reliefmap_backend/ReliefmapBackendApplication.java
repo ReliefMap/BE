54f2bf5e069521d14b0037a340f82ea7facf380d
hyunn522 <tjgus52213@gmail.com>
2025-08-16 17:02:17 +0900

    initial commit

---
+++ src/main/java/com/fisa/reliefmap_backend/ReliefmapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fisa.reliefmap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReliefmapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReliefmapBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fisa/reliefmap_backend/ReliefmapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisa.reliefmap_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReliefmapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
